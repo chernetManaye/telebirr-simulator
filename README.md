2880582b64cbc7193b587de9e4f5e6b87e63a294
Chernet Manaye <chernetmanaye860@gmail.com>
2026-01-14 01:46:41 -0800

    clean code

---
+++ README.md
@@ -0,0 +1,9 @@
+The HTML5 page is accessable with this url
+http://localhost:3000/web/?
+appid=1072905731584000&
+merch_code=000000&
+nonce_str=abc123xyz&prepay_id=86342a3db7ba01deabee67efa619349482e9bd06&timestamp=1705201234567&
+sign=dummy-signature-for-test&
+sign_type=SHA256WithRSA&
+version=1.0&
+trade_type=Checkout
--- index.js
+++ index.js
@@ -16,15 +16,12 @@ const app = express();
 
 const server = http.createServer(app);
 
-// https://telebirr-node-simulator.onrender.com
-// https://telebirr-node-simulator.onrender.com/web
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/payment/web", express.static(path.join(__dirname, "public")));
-app.use("/payment/web/pay", payRoute);
-app.use("/payment/web/complete", completePaymentRoute);
+app.use("/web", express.static(path.join(__dirname, "public")));
+app.use("/payment/pay", payRoute);
+app.use("/payment/complete", completePaymentRoute);
 app.use("/payment/v1/token", fabricTokenRoute);
 app.use("/payment/v1/merchant/preOrder", preOrderRoute);
 app.use("/payment/v1/merchant/refund", refundOrderRoute);
